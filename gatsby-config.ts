55edff1681243a06d29d20b50bf18048fea13fd6
Valentine C <valentine.git@me.vc.sg>
2022-05-09 07:02:14 +0800

    Add PostCSS and Tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,6 +18,7 @@ const config: GatsbyConfig = {
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
