3a28932f4ff3d9fb0ac30149262498b060c9619f
Valentine C <valentine.git@me.vc.sg>
2022-05-12 02:14:00 +0800

    Add Lato, Roboto Mono fonts

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,1 +1,4 @@
 import './src/styles/global.css';
+
+import '@fontsource/lato';
+import '@fontsource/roboto-mono';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /* eslint-disable global-require */
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lato', defaultTheme.fontFamily.sans],
+        mono: ['Roboto Mono', defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
